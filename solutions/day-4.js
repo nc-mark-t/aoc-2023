9ecdffd691b16072c41eeb3500a30bc122f87fd5
Mark Tyree <nc-mark-t>
2023-12-04 10:19:00 +0000

    part 2

--- solutions/day-4.js
+++ solutions/day-4.js
@@ -4,16 +4,18 @@ const games = readAndSplit(4)
   .map((game) => game.replace(/Card \d: /, ""))
   .map((game) => game.split(" | "));
 let total = 0;
-games.forEach((game) => {
+const scratchCards = games.map((_, index) => index);
+while (scratchCards.length) {
+  total++;
+  const gameNumber = scratchCards.pop();
+  const game = games[gameNumber];
   const elfsNumbers = game[0].split(" ");
   const winningNumbers = game[1].split(" ").filter((num) => num !== "");
   const winners = elfsNumbers.filter((number) => {
     return winningNumbers.includes(number);
   });
-  console.log(elfsNumbers, winningNumbers);
-  if (winners.length) {
-    console.log(winners.length);
-    total += 2 ** (winners.length - 1);
+  for (let i = 0; i < winners.length; i++) {
+    scratchCards.push(i + 1 + gameNumber);
   }
-});
+}
 console.log(total);
